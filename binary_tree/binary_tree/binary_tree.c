72c4a0effb5d2f2e8df3736983d108127c1bf45e
unknown <eric71804122@gmail.com>
2022-05-24 04:03:54 +0900

    d

--- binary_tree/binary_tree/binary_tree.c
+++ binary_tree/binary_tree/binary_tree.c
@@ -1,28 +1,73 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h> 
+char name_binary_tree[16][15] = { "","Dusty","Honey","Brandy","Brunhide","Terry","Coyote","Nugget","Gill","Tansey","Tweed","Zoe","Crocus","Primrose","Nous","Belle" };
+
+void preorder(int k)
+{
+	
+	if (k > 15) {
+		return;
+	}
+	printf("%s, ", name_binary_tree[k]);
+	preorder(k * 2);
+	preorder(k * 2 + 1);
+
+}
 
 void main()
 {
-	char name_binary_tree[16][11] = { NULL,"Dusty","Honey Bear","Brandy","Brunhide","Terry","Coyote","Nugget","Gill","Tansey","Tweed","Zoe","Crocus","Primrose","Nous","Belle" };
-	char s1[15];
+	
+	char s1[15]="";
+	char s2[15]="";
+	char a;
+
+	
 
 	while (1) {
-		char s1[15];
-		printf("?");
-		scanf("%s", s1);
-
-		if (strstr(s1, "A(") != NULL) {
-			printf("A");
+		
+		printf("?\n");
+		scanf("%s",s1);
+		if (strstr(s1, "END") != NULL) {
+		break;
 		}
-		else if (strstr(s1, "D(") != NULL) {
-
+		int c = 0;
+		a = s1[0];
+		for (int i = 2; i < strlen(s1)-1; i++) {
+			s2[c] = s1[i];
+			c++;
 		}
-		else if (strstr(s1, "R(") != NULL) {
-
+		s2[c] = '\0';
+		int jj;
+		for (jj = 0; jj < 16; jj++) {
+			if (strcmp(name_binary_tree[jj], s2)==0) {
+				printf("%d\n", jj);
+				break;
+			}
+		
 		}
-		else if (strstr(s1, "END") != NULL) {
-			break;
+
+		
+
+		if (a == 'A') {
+			if (jj > 7 && jj < 16) {
+				printf("UnKnown\n");
+			}
+			preorder(jj*2);
+			preorder(jj * 2+1);
+		}
+		else if (a == 'D') {
+			if (jj ==1) {
+				printf("UnKnown");
+			}
+			while (jj>1.9) {
+				jj = jj / 2;
+				printf("%s, ", name_binary_tree[jj]);
+			}
+			printf("\n");
+		}
+		else if (a == 'R') {
+
 		}
 	}
-
 }
